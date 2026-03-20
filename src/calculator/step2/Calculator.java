1470f0fcd9d65e44436ff80da12d2f74b349512f
tjdgns0618 <gruddufs@gmail.com>
2026-03-20 11:13:22 +0900

    add class divide method

--- src/calculator/step2/Calculator.java
+++ src/calculator/step2/Calculator.java
@@ -1,4 +1,35 @@
 package calculator.step2;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Calculator {
+
+    private ArrayList<Integer> results = new ArrayList<>();
+
+    public int calculate(int firstNum, int secondNum, char operation){
+        switch (operation){
+            case '+':
+                return (firstNum + secondNum);
+            case '-':
+                return (firstNum - secondNum);
+            case '*':
+                return (firstNum * secondNum);
+            case '/':
+                return (firstNum / secondNum);
+            default:
+                System.out.println("사칙연산이 아닌 값이 들어가 있습니다.");
+                break;
+        }
+        return 0;
+    }
+
+    public ArrayList<Integer> getResults() {
+        return results;
+    }
+
+    public void setResults(int result) {
+        this.results.add(result);
+    }
 }
