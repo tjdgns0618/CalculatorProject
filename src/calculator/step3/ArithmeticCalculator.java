1fb3113a3f80e88f7f8a40d1407eab0d1c14e0d8
tjdgns0618 <gruddufs@gmail.com>
2026-03-20 12:34:38 +0900

    step3 init

---
+++ src/calculator/step3/ArithmeticCalculator.java
@@ -0,0 +1,9 @@
+package calculator.step3;
+
+public class ArithmeticCalculator<T> {
+    private T result;
+
+    public T calculate(){
+        return this.result;
+    }
+}
---
+++ src/calculator/step3/Main.java
@@ -0,0 +1,10 @@
+package calculator.step3;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
