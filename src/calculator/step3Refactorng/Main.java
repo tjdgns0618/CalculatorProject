9749adf3eef46ebb6d4770bd8a8385ecbb87afd7
tjdgns0618 <gruddufs@gmail.com>
2026-03-23 17:32:30 +0900

    update Main

--- src/calculator/step3Refactorng/Main.java
+++ src/calculator/step3Refactorng/Main.java
@@ -18,14 +18,14 @@ public class Main {
             System.out.println("계산 결과: " + result);
             System.out.println("저장된 결과: " + arithmeticCalculator.getResults() + "\n");
 
-            System.out.println("계산을 계속 하시겠습니까? (N : 종료, remove : 삭제 모드, 아무거나 입력시 다시 계산, search : 조회)");
+            System.out.println("계산을 계속 하시겠습니까? (n : 종료, remove : 삭제 모드, 아무거나 입력시 다시 계산, search : 조회)");
             String remove = input.inputAgain();
 
             switch (remove){
-                case "n" :
+                case "n" : case "N": case "No": case "no":
                     System.out.println("프로그램을 종료합니다.");
                     return;
-                case "remove":
+                case "remove": case "Remove":
                     while(true){
                         System.out.println("삭제하시겠습니까? (yes 입력시 첫 결과값 삭제, no 입력 시 삭제 모드 종료)");
                         System.out.println("저장된 결과: " + arithmeticCalculator.getResults());
@@ -40,7 +40,7 @@ public class Main {
                         }
                     }
                     break;
-                case "search":
+                case "search": case "Search":
                     System.out.println("몇보다 높은 숫자를 찾을건가요?");
                     double number = input.inputNumber("숫자를 입력해주세요: ");
                     System.out.println("결과: " + arithmeticCalculator.getSearch(number));
