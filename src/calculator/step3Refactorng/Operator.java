1dd7ac4a98e20bb0855f2c1da9b9c87c770b7599
tjdgns0618 <gruddufs@gmail.com>
2026-03-25 17:57:22 +0900

    refactor step3Refactoring/Operator biFunction

--- src/calculator/step3Refactorng/Operator.java
+++ src/calculator/step3Refactorng/Operator.java
@@ -1,15 +1,26 @@
 package calculator.step3Refactorng;
 
+import java.util.function.BiFunction;
+
 public enum Operator {
-    PLUS('+'),
-    MINUS('-'),
-    MULTIPLY('*'),
-    DIVIDE('/');
+    // 계속해서 "람다를 메서드 참조로 바꿀 수 있음" 라는 오류가 떠서 찾아보니
+    // 이미 존재하는 식이 있으니 사용하라는 추천이였다. (a, b) -> a + b == Double::sum
+    PLUS('+', Double::sum),
+    MINUS('-', (a,b) -> a - b),
+    MULTIPLY('*',(a,b) -> a * b ),
+    DIVIDE('/', (a,b) -> Math.round(a / b * 100) / 100.0);
 
     private final char operation;
 
-    Operator(char operation) {
+    private final BiFunction<Double, Double, Double> expression;
+
+    Operator(char operation, BiFunction<Double, Double, Double> expression) {
         this.operation = operation;
+        this.expression = expression;
+    }
+
+    public double calculate(double num1, double num2) {
+        return expression.apply(num1, num2);
     }
 
     public static Operator getOperator(char operation) {   // static 함수로 만들어서 사용
